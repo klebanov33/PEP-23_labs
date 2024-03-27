a149bd1d1aa7437a7b1b3ed37953dccae5342b3c
klebanov33 <31345284+klebanov33@users.noreply.github.com>
2024-03-27 21:35:30 +0200

    Add files via upload

---
+++ PE1/Module_2/PE1-2.5.1.2.py
@@ -0,0 +1,6 @@
+hours = 3 # number of hours
+seconds = 3600 # number of seconds in 1 hour
+
+print("Hours: ", hours) #printing the number of hours
+print("Seconds in Hours: ", hours * seconds) # printing the number of seconds in a given number of hours
+print("Goodbye")
