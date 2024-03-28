39f633b05b3288ee69da74b5a5a6c118d74ab459
klebanov33 <31345284+klebanov33@users.noreply.github.com>
2024-03-28 19:10:02 +0200

    Add files via upload

---
+++ PE1/Module_3/PE1-3.2.1.9.py
@@ -0,0 +1,8 @@
+word = input("Enter a word: ")
+while True:
+    word != "chupacabra"
+    if word == "chupacabra":
+        break
+    else:
+        word = input("Enter a word: ")
+print("You've successfully left the loop.")
