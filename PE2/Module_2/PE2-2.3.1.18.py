30f4f14321cce8ce58408206207173b2d07de47c
klebanov33 <31345284+klebanov33@users.noreply.github.com>
2024-06-24 21:26:14 +0300

    Update PE2-2.3.1.18.py

--- PE2/Module_2/PE2-2.3.1.18.py
+++ PE2/Module_2/PE2-2.3.1.18.py
@@ -1,29 +1,29 @@
 def mysplit(strng):
     new_list = []
-    a = ""
+    splited_word = ""
+    count_symbol = 0
     for ch in strng:
+        count_symbol += 1
         if ch != "" and ch != " ":
-            a += ch
-        if ch == " " and a != "":
-            new_list.append(a)
-            a = ""
-        else:
+            splited_word += ch
+        if (count_symbol == len(strng)) and splited_word != "":
+            new_list.append(splited_word)
+            splited_word = ""
+        if (count_symbol == len(strng)) and splited_word == "":    
+            break
+        if ch == " " and splited_word != "":
+            new_list.append(splited_word)
+            splited_word = ""
+        if ch == " " and splited_word == "":
             continue
     return new_list
-
-
-print(mysplit("To be or not to be, that is the question"))
-print(mysplit("To be or not to be,that is the question"))
-print(mysplit("   "))
-print(mysplit(" abc "))
-print(mysplit(""))
 
 
 ### OUTPUT ###
 
 '''
-['To', 'be', 'or', 'not', 'to', 'be,', 'that', 'is', 'the']
-['To', 'be', 'or', 'not', 'to', 'be,that', 'is', 'the']
+['To', 'be', 'or', 'not', 'to', 'be,', 'that', 'is', 'the', 'question']
+['To', 'be', 'or', 'not', 'to', 'be,that', 'is', 'the', 'question']
 []
 ['abc']
 []
